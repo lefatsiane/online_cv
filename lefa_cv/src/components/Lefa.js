7f82725863cbd85edf0e7faaa1995f32f0f3240b
lefatsiane <154235500+lefatsiane@users.noreply.github.com>
2024-06-20 12:20:27 +0200

    added projects pic to home page

--- lefa_cv/src/components/Lefa.js
+++ lefa_cv/src/components/Lefa.js
@@ -27,30 +27,57 @@
 //   }
 // }
 
-import React from "react";
+// import React from "react";
 
-const Lefa = () => {
-  return (
-    <div className="lefaPage">
-      <div className="lefaText">
-        <h1 className="lefaH1">Welcome to Lefa</h1>
-        <h2 className="lefaSlogan">A Dedicated Web Developer</h2>
-        <div className="lefaApps">
-          HTML | CSS | JavaScript | MongoDB | Express | React | Node | Bootstrap
+// const Lefa = () => {
+//   return (
+//     <div className="lefaPage">
+//       <div className="lefaText">
+//         <h1 className="lefaH1">Welcome to Lefa</h1>
+//         <h2 className="lefaSlogan">A Dedicated Web Developer</h2>
+//         <div className="lefaApps">
+//           HTML | CSS | JavaScript | MongoDB | Express | React | Node | Bootstrap
+//         </div>
+//       </div>
+
+//       <div className="img2Div">
+//         <img
+//           className="img2"
+//           src="./img3.jpg"
+//           alt="Lefa wearing a shirt and tie"
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Lefa;
+
+import React, { Component } from "react";
+
+
+export default class Lefa extends Component {
+  render() {
+    return (
+      <div className="lefaPAge">
+        <div className="lefaText">
+          <div className="lefaH2Div">
+            <h2 className="projectH2">Welcome to Lefa </h2>
+          </div>
+          <div className="cardDiv">
+            
+          </div>
+          <div className="projectSlogan">A few projects I have worked on</div>
+        </div>
+
+        <div className="img4Div">
+          <img
+            className="img4"
+            src="./img4.jpg"
+            alt="lefa in the background wearing a shirt and tie"
+          ></img>
         </div>
       </div>
-
-      {/* <div className="img2Div">
-        <img
-          className="img2"
-          src="./img3.jpg"
-          alt="Lefa wearing a shirt and tie"
-        />
-      </div> */}
-    </div>
-  );
-};
-
-export default Lefa;
-
-
+    );
+  }
+}
